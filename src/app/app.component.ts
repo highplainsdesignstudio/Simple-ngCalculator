79fe4ea7a0e388aa44b1f07dd07fd56c59e01667
Michael <hpds.life@gmail.com>
2018-07-27 09:14:52 -0600

    Bootstrap styles added. Calculator frame started. v0.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  number1 = "";
+  number2 = "";
+  currentNumber = "";
+
+  numberClicked = function(evt, num) {
+    // console.log testing.
+    console.log(num);
+    // let numberString = this.currentNumber + String(num);
+
+    this.currentNumber += String(num);
+    console.log(this.currentNumber);
+
+    // This is NOT the proper way to introduce elements using Angular. This
+    // MUST be changed after the prototype is complete.
+    // document.getElementById("primary-input").value = this.currentNumber;
+  } // end numberClicked()
+
+
+
+
+  equals = function() {
+
+  }
 }
