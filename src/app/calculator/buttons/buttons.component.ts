6e8dedbc66132ca6d2f8034af2371dff4864dc68
highplainsdesignstudio <admin@highplainsdesignstudio.com>
2018-08-29 19:35:54 -0600

    v0.0.5 ButtonsComponent creates strings from button clicks.

--- src/app/calculator/buttons/buttons.component.ts
+++ src/app/calculator/buttons/buttons.component.ts
@@ -10,17 +10,30 @@ export class ButtonsComponent implements OnInit {
   numbers: number[] = [1,2,3,4,5,6,7,8,9,0];
   functions: string[] = ['.','*','/','+','-','C','='];
 
+  display = "";
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onFunctionClick(funct) {
-    console.log(funct);
+    if(funct === ".") {
+      this.display += funct;
+    } else if(funct === "C") {
+      this.display = "";
+    } else if(funct === "=") {
+      var y = eval(this.display);
+      this.display = y.toString();
+    } else {
+      this.display = this.display + " " + funct + " ";
+    }
+    console.log(this.display);
   }
 
   onNumberClick(num) {
-    console.log(num.toString());
+    this.display += num.toString();
+    console.log(this.display);
   }
 
 }
